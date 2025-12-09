b9bbfd4f3a34ef30b935acfd6b6ab20bac43e667
salmanwheep <salmanwheep@gmail.com>
2025-12-09 20:43:18 +0300

    add productEntity

--- lib/src/features/product/domain/entities/product.dart
+++ lib/src/features/product/domain/entities/product.dart
@@ -2,6 +2,24 @@ import '../../data/models/product.dart';
 
 class ProductEntity {
   ProductEntity();
+  final int id;
+  final String title;
 
+  final double price;
+  final double rating;
+  final String shippingInformation;
+  final int reviewsCount;
+  
+  final String image;
+
+  ProductEntity({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.rating,
+    required this.shippingInformation,
+    required this.reviewsCount,
+    required this.image,
+  }); 
   ProductModel get toModel => ProductModel();
 }
