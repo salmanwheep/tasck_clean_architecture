1e069e521b62818fd5dbdbf0a8e5af7324f8fbd0
salmanwheep <salmanwheep@gmail.com>
2025-12-15 12:38:32 +0300

    add state_product

--- lib/src/features/product/presentation/bloc/product_state.dart
+++ lib/src/features/product/presentation/bloc/product_state.dart
@@ -3,12 +3,27 @@ part of 'product_bloc.dart';
 sealed class ProductState {}
 
 final class ProductInitial extends ProductState {}
+
 final class ProductLoading extends ProductState {}
-final class ProductLoaded extends ProductState {
-  final String data;
-  ProductLoaded({required this.data});
-}
+
 final class ProductError extends ProductState {
   final String message;
-  ProductError(this.message);
+  ProductError({required this.message});
+}
+
+class ProductStateData extends ProductState {
+  final List<ProductEntity> products;
+  ProductStateData({required this.products});
+}
+
+final class ProductLoaded extends ProductStateData {
+  ProductLoaded({required super.products});
+}
+
+final class ProductLoadedGridView extends ProductStateData {
+  ProductLoadedGridView({required super.products});
+}
+
+final class ProductLoadedListView extends ProductStateData {
+  ProductLoadedListView({required super.products});
 }
