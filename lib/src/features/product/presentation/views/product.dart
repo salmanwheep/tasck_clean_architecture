bbaf4fb89e609321ebe115eced5fe6eb20f4a40b
salmanwheep <salmanwheep@gmail.com>
2025-12-15 12:40:55 +0300

    add view_product

--- lib/src/features/product/presentation/views/product.dart
+++ lib/src/features/product/presentation/views/product.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tasck_clean_architecture/src/features/product/presentation/views/components/loadedWidget.dart';
 import '../bloc/product_bloc.dart';
 
 class ProductView extends StatelessWidget {
@@ -9,24 +10,35 @@ class ProductView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Product')),
-      body: BlocBuilder<ProductBloc, ProductState>(
-        builder: (context, state) {
-          final bloc = context.watch<ProductBloc>();
-          if (state is ProductLoading) {
-            return Center(child: CircularProgressIndicator());
-          } else if (state is ProductLoaded) {
-            return Center(child: Text(state.data));
-          } else if (state is ProductError) {
-            return Center(child: Text(state.message, style: TextStyle(color: Colors.red)));
-          }
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return BlocBuilder<ProductBloc, ProductState>(
+      builder: (context, state) {
+        final bloc = context.watch<ProductBloc>();
+        if (state is ProductLoading) {
+          return Center(child: CircularProgressIndicator());
+        } else if (state is ProductLoaded) {
           return Center(
-            child: ElevatedButton(
-              onPressed: () => bloc.add(FetchProduct()),
-              child: Text('Fetch Data'),
-            ),
+            child: Loadedwidget(productList: state.products, isList: true),
           );
-        },
-      ),
+        } else if (state is ProductError) {
+          return Center(
+            child: Text(state.message, style: TextStyle(color: Colors.red)),
+          );
+        } else if (state is ProductLoadedGridView) {
+          return Center(
+            child: Loadedwidget(productList: state.products, isList: false),
+          );
+        } else if (state is ProductLoadedListView) {
+          return Center(
+            child: Loadedwidget(productList: state.products, isList: true),
+          );
+        }
+        return Center(child: Text('Fetch Data'));
+      },
     );
   }
 }
