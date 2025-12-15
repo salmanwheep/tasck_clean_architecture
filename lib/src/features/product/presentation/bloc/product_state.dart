879c8aa853c8a8008bed284d90380cef0a075002
salmanwheep <salmanwheep@gmail.com>
2025-12-15 21:25:50 +0300

    add equatable to ProductEvent

--- lib/src/features/product/presentation/bloc/product_state.dart
+++ lib/src/features/product/presentation/bloc/product_state.dart
@@ -1,6 +1,9 @@
 part of 'product_bloc.dart';
 
-sealed class ProductState {}
+sealed class ProductState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 final class ProductInitial extends ProductState {}
 
