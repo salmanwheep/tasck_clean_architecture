a6c53c500144c30dd4252b1fa4dc353ec1e9b903
salmanwheep <salmanwheep@gmail.com>
2025-12-15 12:32:40 +0300

    add models_product

--- lib/src/features/product/data/models/product.dart
+++ lib/src/features/product/data/models/product.dart
@@ -1,3 +1,35 @@
 import '../../domain/entities/product.dart';
 
-class ProductModel extends ProductEntity {}
+class ProductModel extends ProductEntity {
+  const ProductModel({
+    required super.id,
+    required super.title,
+    required super.price,
+    required super.rating,
+    required super.shippingInformation,
+    required super.reviewsCount,
+    required super.image,
+  });
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'],
+      rating: json['rating'],
+      shippingInformation: json['shippingInformation'],
+      reviewsCount: json['reviews'].length,
+      image: json['images'].first,
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'price': price,
+      'rating': rating,
+      'shippingInformation': shippingInformation,
+      'reviews': reviewsCount,
+      'image': image,
+    };
+  }
+}
