e1d1ba4193145d45589f69f31bbcbc629495150d
salmanwheep <salmanwheep@gmail.com>
2025-12-15 12:38:44 +0300

    add event_product

--- lib/src/features/product/presentation/bloc/product_event.dart
+++ lib/src/features/product/presentation/bloc/product_event.dart
@@ -1,5 +1,19 @@
 part of 'product_bloc.dart';
 
-sealed class ProductEvent {}
+sealed class ProductEvent extends Equatable {
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
 
-final class FetchProduct extends ProductEvent {}
+final class FetchProduct extends ProductEvent {
+  final int current;
+  FetchProduct({required this.current});
+}
+
+final class RefreshProduct extends ProductEvent {}
+
+final class SwichedProduct extends ProductEvent {
+  final bool isList;
+  final List<ProductEntity> products;
+  SwichedProduct({required this.isList, required this.products});
+}
