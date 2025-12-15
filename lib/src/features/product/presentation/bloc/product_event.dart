22acb67ba179b81d04619cec1b1be7784c7318c7
salmanwheep <salmanwheep@gmail.com>
2025-12-15 23:12:04 +0300

    update product_event

--- lib/src/features/product/presentation/bloc/product_event.dart
+++ lib/src/features/product/presentation/bloc/product_event.dart
@@ -7,18 +7,35 @@ sealed class ProductEvent extends Equatable {
 }
 
 final class FetchProduct extends ProductEvent {
-  final int current;
-  const FetchProduct({required this.current});
+  // final int current;
+  const FetchProduct(
+    // {required this.current}
+  );
   @override
-  List<Object?> get props => [current];
+  List<Object?> get props => [
+    // current
+  ];
 }
 
-final class RefreshProduct extends ProductEvent {}
+final class RefreshProduct extends ProductEvent {
+  @override
+  List<Object?> get props => [];
+}
 
 final class SwichedProduct extends ProductEvent {
-  final bool isList;
-  final List<ProductEntity> products;
-  const SwichedProduct({required this.isList, required this.products});
+  // final bool isList;
+  // final List<ProductEntity> products;
+  const SwichedProduct(
+    // {required this.isList, required this.products}
+  );
+
   @override
-  List<Object?> get props => [isList, products];
+  List<Object?> get props => [
+    // isList, products
+  ];
+}
+
+final class LoadingMore extends ProductState {
+  @override
+  List<Object?> get props => [];
 }
