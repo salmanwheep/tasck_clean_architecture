a2e78f506c09f2acacea97cc98e3092974eecc49
salmanwheep <salmanwheep@gmail.com>
2025-12-15 12:31:41 +0300

    add product_impl

--- lib/src/features/product/data/repositories/product_impl.dart
+++ lib/src/features/product/data/repositories/product_impl.dart
@@ -1,3 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:tasck_clean_architecture/core/error/exception.dart';
+import 'package:tasck_clean_architecture/core/error/failure.dart';
+import 'package:tasck_clean_architecture/core/network/network_info.dart';
+import 'package:tasck_clean_architecture/src/features/product/domain/entities/product.dart';
+
 import '../../domain/repositories/product.dart';
 import '../../data/sources/local/product.dart';
 import '../../data/sources/remote/product.dart';
@@ -5,6 +11,30 @@ import '../../data/sources/remote/product.dart';
 class ProductRepositoryImpl implements ProductRepository {
   final ProductLocalService _localService;
   final ProductRemoteService _remoteService;
-
-  ProductRepositoryImpl(this._localService, this._remoteService);
+  final NetworkInfo _networkInfo;
+  ProductRepositoryImpl(
+    this._localService,
+    this._remoteService,
+    this._networkInfo,
+  );
+  @override
+  Future<Either<Failure, List<ProductEntity>>> fetchProducts(
+    int current,
+  ) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        final cache = await _remoteService.getProducts(current);
+        _localService.cacheProducts(cache);
+        return Right(cache);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        return Right(await _localService.getProducts());
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
+  }
 }
