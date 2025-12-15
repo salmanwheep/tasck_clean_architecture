6c23b925553d2aae10e43bd3d4099a65b44747fe
salmanwheep <salmanwheep@gmail.com>
2025-12-15 21:30:11 +0300

    add equatable to ProductState

--- lib/src/features/product/presentation/bloc/product_state.dart
+++ lib/src/features/product/presentation/bloc/product_state.dart
@@ -5,28 +5,49 @@ sealed class ProductState extends Equatable {
   List<Object?> get props => [];
 }
 
-final class ProductInitial extends ProductState {}
+final class ProductInitial extends ProductState {
+  @override
+  List<Object?> get props => [];
+}
 
-final class ProductLoading extends ProductState {}
+final class ProductLoading extends ProductState {
+  @override
+  List<Object?> get props => [];
+}
 
 final class ProductError extends ProductState {
+  @override
+  List<Object?> get props => [];
+
   ProductError({required this.message});
   final String message;
 }
 
 class ProductStateData extends ProductState {
+  @override
+  List<Object?> get props => [];
+
   final List<ProductEntity> products;
   ProductStateData({required this.products});
 }
 
 final class ProductLoaded extends ProductStateData {
+  @override
+  List<Object?> get props => [];
+
   ProductLoaded({required super.products});
 }
 
 final class ProductLoadedGridView extends ProductStateData {
+  @override
+  List<Object?> get props => [];
+
   ProductLoadedGridView({required super.products});
 }
 
 final class ProductLoadedListView extends ProductStateData {
+  @override
+  List<Object?> get props => [];
+
   ProductLoadedListView({required super.products});
 }
