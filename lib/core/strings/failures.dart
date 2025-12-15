17aecb1f6ec304d1e878c091604e39427afe7866
salmanwheep <salmanwheep@gmail.com>
2025-12-15 12:35:47 +0300

    add local_product

---
+++ lib/core/strings/failures.dart
@@ -0,0 +1,3 @@
+const String SERVER_FAILURE_MESSAGE = "Please try again later";
+const String OFFLINE_FAILURE_MESSAGE = "Please Check your Internet Connection";
+const String Cache_FAILURE_MESSAGE = "No Data";
--- lib/src/features/product/data/sources/local/product.dart
+++ lib/src/features/product/data/sources/local/product.dart
@@ -1,5 +1,33 @@
-abstract class ProductLocalService {}
+import 'package:dartz/dartz.dart';
+import 'package:tasck_clean_architecture/core/db_helper.dart';
+import 'package:tasck_clean_architecture/src/features/product/data/models/product.dart';
+
+abstract class ProductLocalService {
+  Future<List<ProductModel>> getProducts();
+  Future<Unit> cacheProducts(List<ProductModel> products);
+}
 
 class ProductLocalServiceImpl implements ProductLocalService {
-  ProductLocalServiceImpl();
+  final DatabaseHelper databaseHelper;
+
+  ProductLocalServiceImpl({required this.databaseHelper});
+
+  @override
+  Future<List<ProductModel>> getProducts() async {
+    final userMaps = await databaseHelper.queryAll('product');
+
+    return userMaps
+        .map((productJson) => ProductModel.fromJson(productJson))
+        .toList();
+  }
+
+  @override
+  Future<Unit> cacheProducts(List<ProductModel> products) async {
+    products.forEach((p) async {
+      await databaseHelper.insert(p.toJson(), "product");
+    });
+    return Future.value(unit);
+
+    // throw UnimplementedError();
+  }
 }
