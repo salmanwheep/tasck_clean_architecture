e44bcbf9481742d36cff730566529a1ace33fd0e
salmanwheep <salmanwheep@gmail.com>
2025-12-15 12:30:10 +0300

    add failure

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class OfflineFailure extends Failure {
+  OfflineFailure();
+}
+
+class ServerFailure extends Failure {
+  ServerFailure();
+}
+
+class CacheFailure extends Failure {
+  CacheFailure();
+}
