22cf4605221aae1592310022a65f25f52e0aa31d
salmanwheep <salmanwheep@gmail.com>
2025-12-16 15:41:31 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tasck_clean_architecture/core/db_helper.dart';
 import 'package:tasck_clean_architecture/src/features/product/presentation/bloc/product_bloc.dart';
+import 'package:tasck_clean_architecture/src/features/product/presentation/bloc/product_event.dart';
 import 'package:tasck_clean_architecture/src/features/product/presentation/views/product.dart';
 import 'injection_container.dart' as di;
 
@@ -20,9 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
-          create: (_) => di.sl<ProductBloc>()..add(FetchProduct(current: 0)),
-        ),
+        BlocProvider(create: (_) => di.sl<ProductBloc>()..add(FetchProduct())),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
@@ -42,18 +41,15 @@ class HomeState extends State<Home> {
   bool islist = true;
   @override
   build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text("welcome"),
         actions: [
           IconButton(
             onPressed: () {
-              final d = context.read<ProductBloc>();
-              if (d.state is ProductStateData) {
-                final m = context.read<ProductBloc>().state as ProductStateData;
-                islist = !islist;
-                d.add(SwichedProduct(isList: islist, products: m.products));
+              final productBloc = context.read<ProductBloc>();
+              if (productBloc.state is ProductLoaded) {
+                productBloc.add(SwichedProduct());
               }
             },
             icon: Icon(Icons.sports_basketball),
@@ -61,37 +57,6 @@ class HomeState extends State<Home> {
         ],
       ),
       body: ProductView(),
-      //  Center(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: [
-      //     //   Expanded(
-      //     //     child: FutureBuilder(
-      //     //       future: s.getProducts(),
-      //     //       initialData: [],
-      //     //       builder: (c, snap) => islist
-      //     //           ? ListView.builder(
-      //     //               itemCount: snap.data!.length,
-      //     //               itemBuilder: (v, index) =>
-      //     //                   ProductDetilsCard(model: snap.data![index]),
-      //     //             )
-      //     //           : GridView.builder(
-      //     //               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //     //                 crossAxisCount: 2,
-      //     //               ),
-      //     //               itemCount: snap.data!.length,
-      //     //               itemBuilder: (c, index) => Container(
-      //     //                 child: ProductDetilsCardGrid(
-      //     //                   model: snap.data![index],
-      //     //                 ),
-      //     //               ),
-      //     //             ),
-      //     //     ),
-      //     //   ),
-      //     // ],
-      // ]  ),
-      // ),
     );
   }
 }
-// class HomePage extends StateL
